6cccaf4d47928d6c885b6f49a681cb8f50dc4e2d
Forhad Ahmed <ahmed.7449@gmail.com>
2014-07-21 18:46:57 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+ls = `ls`
+
+print "Number of files: %d" % len(ls.split())
+
+ls = `ls -lrS`
+
+largest = ls.split('\n')[-2] # last line in 'ls -lrS'
+
+columns = largest.split()
+ 
+print "Largest file: %s (%s bytes)" % (columns[8], columns[4])
